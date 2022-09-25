c1ec392300ebbe45807b31a6be3261c88c3470af
felipe.siqueira <felipesiqgodoy@gmail.com>
2022-09-25 17:44:59 -0300

    Add Regex and Messages helpers

---
+++ src/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+  INVALID_PASSWORD:
+    'The password must contain at least one character from each of the following types: lower case letter, upper case letter, number and special character.',
+  INVALID_EMAIL_OR_PASSWORD: 'Invalid email and/or password',
+};
