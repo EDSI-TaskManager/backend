4e78e98a255989e9a7de368d316a2115e96dffac
felipe.siqueira <felipesiqgodoy@gmail.com>
2022-10-17 13:28:51 -0300

    Heroku deploy: staging

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,9 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
